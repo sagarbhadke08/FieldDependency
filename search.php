bb271b225535bf12fd08860b24c6d89f451f2013
Sagar Bhadke <94558820+sagarbhadke08@users.noreply.github.com>
2024-02-24 10:34:18 +0530

    Update search.php

--- search.php
+++ search.php
@@ -9,26 +9,25 @@ $dbname = "Dependency";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Process the search query
+
 if (isset($_GET["friendName"])) {
-    // Get the Friend name from the query parameter
+   
     $friendName = $_GET["friendName"];
 
-    // Prepare and execute the SQL query
     $sql = "SELECT * FROM data WHERE Friend = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $friendName);
     $stmt->execute();
 
-    // Get the result
+
     $result = $stmt->get_result();
 
-    // Display the results
+
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             echo "Name: " . $row["Name"] . "<br>";
